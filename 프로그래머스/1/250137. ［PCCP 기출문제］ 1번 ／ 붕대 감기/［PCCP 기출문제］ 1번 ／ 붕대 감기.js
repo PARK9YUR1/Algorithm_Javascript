7e32a4a9229fcd700ed8a15010d9a6b6bf4cbef0
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-03-14 19:53:03 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 13.98 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,19 +1,5 @@
 function solution(bandage, health, attacks) {
     const [t, x, y] = bandage;
-    // t초 동안 붕대 감기 -> 초당 체력 x 회복
-    // t초 연속 붕대 감기 성공 -> 체력 y 추가회복
-    // 현재체력 <= 최대체력
-    
-    // 기술 도중 몬스터 공격 -> 기술 취소
-    // 공격 당하는 순간 -> 체력 회복 불가!
-    // 기술취소 or 기술종료 -> 붕대감기 다시! => 연속성공시간=0
-    
-    // 공격 -> 피해량만큼 현재체력 감소
-    // 현재체력 <= 0   -->  캐릭터죽음, 더이상 체력회복불가!!
-    
-    // bandage: [시전 시간, 초당 회복량, 추가 회복량]
-    // health: 최대 체력
-    // attacks: [[공격 시간, 피해량], [공격 시간, 피해량], ...]
 
     let curHealth = health;
     const lastAttack = attacks[attacks.length - 1][0];  // 마지막 공격 시간
