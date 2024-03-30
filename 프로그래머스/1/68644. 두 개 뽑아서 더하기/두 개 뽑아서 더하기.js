72e67088f15bd1001d35fe1602ad6f45317b535b
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-03-30 21:35:12 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = [];
+    for (let i=0; i<numbers.length - 1; i++) {
+        for (let j=i+1; j<numbers.length; j++) {
+            answer.push(numbers[i]+numbers[j])
+        }
+    }
+    answer.sort((a, b) => a - b);
+    
+    const result = new Set(answer);
+    return [...result];
+}
