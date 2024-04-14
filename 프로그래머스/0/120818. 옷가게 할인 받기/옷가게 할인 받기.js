0ad9593b97f6f6d5a6ae9a39ba6190bd04d3c80f
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-04-14 10:09:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,13 @@
 function solution(price) {
+    let sale = 1;
+    
     if (price >= 500000) {
-        return ~~(price*0.8);
+        sale = 0.8;
     } else if (price >= 300000) {
-        return ~~(price*0.9);
+        sale = 0.9;
     } else if (price >= 100000) {
-        return ~~(price*0.95);
-    } else {
-        return price;
+        sale = 0.95;
     }
+    
+    return ~~(price*sale);
 }
