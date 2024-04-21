1a17a71b3583802cd56a9364cd0a85b61d52c547
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-04-21 11:35:48 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,15 +1,4 @@
 function solution(dot) {
-    if (dot[0] > 0) {
-        if (dot[1] > 0) {
-            return 1;
-        } else {
-            return 4;
-        }
-    } else {
-        if (dot[1] > 0) {
-            return 2;
-        } else {
-            return 3;
-        }
-    }
+    return (dot[0] > 0) ? ((dot[1] > 0) ? 1 : 4):
+                          ((dot[1] > 0) ? 2 : 3);
 }
