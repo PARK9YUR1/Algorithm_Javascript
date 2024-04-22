ba451c32fb42ac98a71b24a28838355c863d96ea
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-04-22 10:24:50 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -2,9 +2,5 @@ function solution(sides) {
     sides.sort((a, b) => b - a);
     const [l, s1, s2] = sides;
     
-    if (l < s1+s2) {
-        return 1;
-    } else {
-        return 2;
-    }
+    return (l<s1+s2) ? 1 : 2;
 }
