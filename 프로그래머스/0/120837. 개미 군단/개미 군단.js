cbb009f34fda410fdb1758feeac894233292c7e0
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-03 09:07:45 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,21 @@
+function solution(hp) {
+    let answer = 0;
+    
+    const calc = (power) => {
+        const troops = ~~(hp/power);
+        hp -= troops * power;
+        answer += troops;
+    }
+    
+    while (hp) {
+        if (hp >= 5) {          // 장군개미
+            calc(5);
+        } else if (hp >= 3) {   // 병정개미
+            calc(3);
+        } else {                // 일개미
+            calc(1);
+        }
+    }
+    
+    return answer;
+}
