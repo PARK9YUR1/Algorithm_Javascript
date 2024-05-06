1c4ec6b9057db6b3faf29b0c4e7231c181211e1e
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-06 21:36:38 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,5 +1,7 @@
 function solution(numbers, n) {
-    return numbers.reduce((acc, cur) => {
+    // return numbers.reduce((acc, cur) => (acc > n) ? acc : acc + cur);
+    
+    return numbers.reduce((acc, cur) => { 
         if (acc > n) {
             return acc;
         } else {
