1dbfbfa5926f09b09da1fe67e3741bcc92c542bb
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-23 10:23:00 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.58 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,16 +1,12 @@
 function solution(numLog) {
     let answer = "";
     
-    const calc = (n) => {
-        let str = (n === 1 ? "w" :
-                   n === -1 ? "s" :
-                   n === 10 ? "d" :
-                              "a")  // n = -10
-        answer += str;
+    const str = {
+        "1": "w", "-1": "s", "10": "d", "-10": "a"
     }
     
     for (let i=1; i<numLog.length; i++) {
-        calc(numLog[i] - numLog[i-1]);
+        answer += str[(numLog[i] - numLog[i-1]).toString()];
     }
     
     return answer;
