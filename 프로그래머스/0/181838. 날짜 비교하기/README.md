462cdd42fda0e2a70acf5900db0cac88c978296d
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-23 12:26:47 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/README.md
+++ 프로그래머스/0/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 12:14:22
+2024년 05월 23일 12:26:44
 
 ### 문제 설명
 
--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,18 +1,16 @@
 function solution(date1, date2) {
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    
-    if (year1 < year2) {
-        return 1;
-    } else if (year1 === year2) {
-        if (month1 < month2) {
-            return 1;
-        } else if (month1 === month2) {
-            if (day1 < day2) {
-                return 1;
+    const stringDate = (date) => {
+        return date.map((dt, i) => {
+            if (i) {  // month, day
+                return dt.toString().padStart(2, "0");
+            } else {  // year
+                return dt.toString().padStart(5, "0");
             }
-        }
+        }).join("");
     }
     
-    return 0;
+    date1 = stringDate(date1);
+    date2 = stringDate(date2);
+    
+    return +(date1 < date2);
 }
