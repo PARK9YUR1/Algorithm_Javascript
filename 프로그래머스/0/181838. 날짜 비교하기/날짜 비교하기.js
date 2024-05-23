ce1dac82b021ea489c7e52ed2bc505ae97d05c55
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-23 12:14:24 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -2,21 +2,17 @@ function solution(date1, date2) {
     const [year1, month1, day1] = date1;
     const [year2, month2, day2] = date2;
     
-    if (year1 > year2) {
-        return 0;
-    } else if (year1 < year2) {
+    if (year1 < year2) {
         return 1;
-    } else {
-        if (month1 > month2) {
-            return 0;
-        } else if (month1 < month2) {
+    } else if (year1 === year2) {
+        if (month1 < month2) {
             return 1;
-        } else {
-            if (day1 >= day2) {
-                return 0;
-            } else {
+        } else if (month1 === month2) {
+            if (day1 < day2) {
                 return 1;
             }
         }
     }
+    
+    return 0;
 }
