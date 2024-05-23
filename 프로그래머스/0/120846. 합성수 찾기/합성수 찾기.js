d9dcd570625e1dc507c8e02868b3128f2d47bc6f
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-23 11:03:33 +0900

    [level 0] Title: 합성수 찾기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const isCompositeNumber = (num) => {
+        let [cnt, n] = [0, num];
+        while (n) {
+            if (num % n === 0) { cnt++ }
+            n--;
+        }
+        
+        return cnt >= 3;
+    }
+    
+    return Array.from({length: n}, (_, i) => i+1)
+                .filter(num => isCompositeNumber(num))
+                .length;
+}
