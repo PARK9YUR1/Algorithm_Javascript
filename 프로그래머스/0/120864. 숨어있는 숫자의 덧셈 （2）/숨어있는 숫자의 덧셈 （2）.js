0026e5d074b1c33bce0a350320ce5c41ac933aae
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-24 14:19:15 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,7 @@
 function solution(my_string) {
-    return my_string.split("").map(str => !isNaN(str) ? str : " ").join("").split(" ").reduce((sum, num) => sum + +num, 0);
+    return my_string.split("")
+                    .map(str => !isNaN(str) ? str : " ")
+                    .join("")
+                    .split(" ")
+                    .reduce((sum, num) => sum + +num, 0);
 }
