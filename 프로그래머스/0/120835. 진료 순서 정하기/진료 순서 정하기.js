29158283109c7d0aeb4f29c8d13b35d07468f324
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-27 12:39:03 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,5 @@
 function solution(emergency) {
-    const arr = emergency.slice().sort((a, b) => b - a);
+    // const arr = emergency.slice().sort((a, b) => b - a);
+    const arr = [...emergency].sort((a, b) => b - a);
     return emergency.map(e => arr.indexOf(e)+1);
 }
