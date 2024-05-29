d9a4945c6f6b3a6067f654df9dd93e4f03d82ca7
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-29 09:43:42 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,10 +1,9 @@
 function solution(n) {
-    let num = n;
     const answer = [];
     
     for (let i=2; i<=n; i++) {
-        while (!(num % i)) {
-            num /= i;
+        while (!(n % i)) {
+            n /= i;
             answer.push(i);
         }
     }
