b678321e83289ec7cde7dbd70a41eccb67b3c2ca
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-29 09:24:55 +0900

    [level 0] Title: 2의 영역, Time: 0.32 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,7 +1,4 @@
 function solution(arr) {
     const [s, e] = [arr.indexOf(2), arr.lastIndexOf(2)];
-    if (s === -1 && e === -1) {
-        return [-1];
-    }
-    return arr.slice(s, e+1);
+    return (s===-1 && e===-1) ? [-1] : arr.slice(s, e+1);
 }
