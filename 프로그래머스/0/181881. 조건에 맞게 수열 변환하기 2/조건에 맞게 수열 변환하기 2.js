75bc1ef9982411cf16f1b7de05a2da50cf00a365
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-29 11:16:19 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,19 +1,22 @@
 function solution(arr) {
     let cnt = 0;
     
-    while (true) {
-        if (arr.filter(num => (num>=50 && !(num%2)) || (num<50 && num%2)).length) {
-            for (let i=0; i<arr.length; i++) {
-                if (arr[i] >= 50 && !(arr[i]%2)) {
-                    arr[i] /= 2;
-                } else if (arr[i] < 50 && arr[i]%2) {
-                    arr[i] = arr[i]*2 + 1;
-                }
+    const check1 = (num) => {
+        return (num >= 50) && !(num % 2);
+    }
+    const check2 = (num) => {
+        return (num < 50) && (num % 2);
+    }
+    
+    while (arr.some(num => check1(num) || check2(num))) {
+        for (let i=0; i<arr.length; i++) {
+            if (check1(arr[i])) {
+                arr[i] /= 2;
+            } else if (check2(arr[i])) {
+                arr[i] = arr[i]*2 + 1;
             }
-            cnt++;
-        } else {
-            break;
         }
+        cnt++;
     }
     
     return cnt;
