d97337f8aa14c7535a2e1fa72d1e706e40fa6e82
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-29 09:33:54 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 7.04 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -3,7 +3,7 @@ function solution(arr, queries) {
         const [s, e, k] = query;
         for (let i=s; i<=e; i++) {
             if (i%k === 0) {
-                arr[i] += 1;
+                arr[i]++;
             }
         }
     }
