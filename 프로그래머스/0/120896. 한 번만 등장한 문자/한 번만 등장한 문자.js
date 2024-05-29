2da6262e075b23390188e9d0fc7bd7e333abc680
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-05-29 10:49:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,12 +1,22 @@
 function solution(s) {
-    const arr = [".", ...s.split("").sort(), "."];
-    
     const answer = [];
-    for (let i=1; i<arr.length-1; i++) {
-        if (arr[i-1] !== arr[i] && arr[i] !== arr[i+1]) {
-            answer.push(arr[i]);
+    
+    for (w of s) {
+        if (s.indexOf(w) === s.lastIndexOf(w)) {
+            answer.push(w);
         }
     }
     
-    return answer.join("");
+    return answer.sort().join("");
+    
+//     const arr = [".", ...s.split("").sort(), "."];
+    
+//     const answer = [];
+//     for (let i=1; i<arr.length-1; i++) {
+//         if (arr[i-1] !== arr[i] && arr[i] !== arr[i+1]) {
+//             answer.push(arr[i]);
+//         }
+//     }
+    
+//     return answer.join("");
 }
