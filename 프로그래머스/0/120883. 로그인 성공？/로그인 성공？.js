d9c5cce4649be086de74e1da247f3167c87261e3
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-02 01:20:29 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,11 @@
 function solution(id_pw, db) {
     const [ID, PW] = id_pw;
+
+    const user = new Map(db);
+    return user.has(ID) ? user.get(ID) === PW ? "login" : "wrong pw" : "fail";
     
-    const correctId = db.filter(([id, pw]) => ID === id);
-    const correctPw = correctId.filter(([id, pw]) => PW === pw);
+    // const correctId = db.filter(([id, pw]) => ID === id);
+    // const correctPw = correctId.filter(([id, pw]) => PW === pw);
     
-    return correctId.length ? correctPw.length ? "login" : "wrong pw" : "fail"; 
+    // return correctId.length ? correctPw.length ? "login" : "wrong pw" : "fail"; 
 }
