2a0549d49c0c7741d1c0944524d9d3f2f76c046d
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-02 00:46:11 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,26 +1,4 @@
 function solution(keyinput, board) {
-    const [N, M] = [parseInt(board[0]/2), parseInt(board[1]/2)];
-    
-    const direction = {
-        "left": [-1, 0], "right": [1, 0],
-        "up": [0, 1], "down": [0, -1]
-    }
-    
-    const game = (pos, dir) => {
-        let [x, y] = pos;
-        const [dx, dy] = direction[dir];
-        const [nx, ny] = [x+dx, y+dy];
-        
-        if (nx < -N || nx > N || ny < -M || ny > M) {
-            return [x, y];
-        }
-        return [nx, ny];
-    }
-    
-    let position = [0, 0];
-    for (key of keyinput) {
-        position = game(position, key);
-    }
-    
-    return position;
+    var answer = [];
+    return answer;
 }
