ace4764545d1ec2110dc35fd5b325f6247faa1ab
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-02 00:31:14 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.99 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,12 +1,17 @@
 function solution(arr, queries) {
-    return queries.map(query => {
-        const [s, e, k] = query;
-        const nums = arr.slice(s, e+1).sort((a, b) => a-b);
-        for (num of nums) {
-            if (num > k) {
-                return num;
-            }
-        }
-        return -1;
-    })
+    // return queries.map(([s, e, k]) => {
+    //     const nums = arr.slice(s, e+1).sort((a, b) => a-b);
+    //     for (num of nums) {
+    //         if (num > k) {
+    //             return num;
+    //         }
+    //     }
+    //     return -1;
+    // })
+    
+    return queries.map(([s, e, k]) => {
+        return arr.slice(s, e+1)
+                  .filter(n => n>k)
+                  .sort((a, b) => a-b)[0] || -1;
+    });
 }
