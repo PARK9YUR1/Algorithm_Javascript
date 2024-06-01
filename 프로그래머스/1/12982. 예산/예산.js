132bac876da4c6cf8d4ad880d624a22973062c10
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-02 01:26:39 +0900

    [level 1] Title: 예산, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,3 +1,7 @@
 function solution(d, budget) {
-    return d.sort((a, b) => a-b).map((v, i) => d.slice(0, i+1).reduce((acc, cur) => acc+cur, 0)).filter(n => n <= budget).length;
+    return d.sort((a, b) => a-b)
+            .map((v, i) => d.slice(0, i+1)
+                            .reduce((acc, cur) => acc+cur, 0))
+            .filter(n => n <= budget)
+            .length;
 }
