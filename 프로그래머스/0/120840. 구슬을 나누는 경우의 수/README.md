306d9d3720cafe2dbebed0730ed024153968416e
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-01 23:35:36 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.17 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 23:28:28
+2024년 06월 01일 23:35:33
 
 ### 문제 설명
 
--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,24 +1,12 @@
 function solution(balls, share) {
-    const f1 = (n) => {
-        if (n === 1) {
+    const factorial = (n) => {
+        if (n === 0 || n === 1) {
             return 1;
         } else {
-            return n * f1(n-1);
-        }
-    }
-    
-    const f2 = (n) => {
-        if (n === share+1) {
-            return share+1;
-        } else {
-            return n * f2(n-1);
+            return n * factorial(n-1);
         }
     }
     
     // balls개 중 share개 뽑기
-    if (balls === share) {
-        return 1;
-    } else {
-        return parseInt(f2(balls) / f1(balls-share));
-    }
+    return Math.round(factorial(balls) / (factorial(balls-share) * factorial(share)));
 }
