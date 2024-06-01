7db5998e1511a9f10139f148c039b909e634fd29
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-02 00:16:33 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,9 +1,11 @@
 function solution(spell, dic) {
-    return dic.filter(d => {
-        let cnt = 0;
-        spell.map(s => {
-            if (d.includes(s)) cnt++;
-        })
-        return cnt === spell.length;
-    }).length ? 1 : 2;
+    // return dic.filter(d => {
+    //     let cnt = 0;
+    //     spell.map(s => {
+    //         if (d.includes(s)) cnt++;
+    //     })
+    //     return cnt === spell.length;
+    // }).length ? 1 : 2;
+    
+    return dic.some(d => spell.every(s => d.includes(s))) ? 1: 2;
 }
