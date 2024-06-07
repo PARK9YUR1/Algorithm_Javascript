914ebbfc2f0213ec58c493d895ac728db74aaf77
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-07 21:03:33 +0900

    [level 1] Title: 최소직사각형, Time: 4.56 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,8 +1,10 @@
 function solution(sizes) {
     let [x, y] = [0, 0];
-    sizes.map(size => size.sort((a, b) => b - a)).forEach(size => {
-        x = Math.max(x, size[0]);
-        y = Math.max(y, size[1]);
+    sizes.forEach(size => {
+        const [w, h] = (size[0] > size[1]) ? size : size.reverse();
+        
+        x = Math.max(x, w);
+        y = Math.max(y, h);
     })
     return x * y;
 }
