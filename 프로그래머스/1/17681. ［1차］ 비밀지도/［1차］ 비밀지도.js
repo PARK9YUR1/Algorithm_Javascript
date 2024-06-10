a3b7a02c8e5e6b88a98138df657ef2c4cd43080b
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-10 19:28:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,20 +1,20 @@
 function solution(n, arr1, arr2) {
     let arr = [];
     
+    const binArr = (num) => {
+        return num.toString(2)
+                  .padStart(n, "0")
+                  .split("")
+                  .map(b => +b);
+    }
+    
     for (let i=0; i<n; i++) {
-        const bin1 = arr1[i].toString(2).padStart(n, "0").split("");
-        const bin2 = arr2[i].toString(2).padStart(n, "0").split("");
+        const b1 = binArr(arr1[i]);
+        const b2 = binArr(arr2[i]);
         
-        let a = "";
-        for (let j=0; j<n; j++) {
-            if (+bin1[j] || +bin2[j]) {
-                a += "#";
-            } else {
-                a += " ";
-            }
-        }
+        const b = b1.map((v, i) => (v || b2[i]) ? "#" : " ").join("");
         
-        arr.push(a);
+        arr.push(b);
     }
     
     return arr;
