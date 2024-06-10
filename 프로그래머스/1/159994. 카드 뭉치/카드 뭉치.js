8c9c20b5c61bd6601444881a38be4f0ced4b2bda
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-11 02:14:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -2,15 +2,8 @@ function solution(cards1, cards2, goal) {
     let [i1, i2] = [-1, -1];
     
     for (word of goal) {
-        if (cards1.includes(word)) {
-            if (cards1[++i1] !== word) {
-                return "No";
-            }
-        } else if (cards2.includes(word)) {
-            if (cards2[++i2] !== word) {
-                return "No";
-            }
-        } else {
+        if ((cards1.includes(word) && cards1[++i1] !== word) 
+            || (cards2.includes(word) && cards2[++i2] !== word)) {
             return "No";
         }
     }
