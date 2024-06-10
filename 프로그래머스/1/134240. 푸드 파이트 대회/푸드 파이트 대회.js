5e5959a1937e41ece20c1e16b0da4d947839da3a
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-10 18:26:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,11 +1,12 @@
 function solution(food) {
     let foods = ["0".repeat(food[0])];
     
-    food.slice(1).reverse().map((cnt, i) => {
-        const fd = food.length - 1 - i;
-        if (cnt > 1) {
-            foods.push(fd.toString().repeat(parseInt(cnt/2)));
-            foods.unshift(fd.toString().repeat(parseInt(cnt/2)));
+    food.slice(1).reverse().map((v, i) => {
+        const [cnt, f] = [parseInt(v/2), food.length - 1 - i];
+        if (v > 1) {
+            const fd = f.toString().repeat(cnt);
+            foods.push(fd);
+            foods.unshift(fd);
         }
     })
     
