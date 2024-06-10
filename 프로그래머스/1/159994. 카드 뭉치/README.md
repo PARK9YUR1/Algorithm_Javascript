28b1eaf7b7de388505eaea36baeea6cdebc89649
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-11 02:12:17 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 11일 02:09:49
+2024년 06월 11일 02:12:13
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,15 @@
 function solution(cards1, cards2, goal) {
-    let [idx1, idx2] = [-1, -1];
+    let [i1, i2] = [-1, -1];
     
     for (word of goal) {
         if (cards1.includes(word)) {
-            const i = cards1.indexOf(word);
-            if (idx1 !== i-1) { return "No"; }
-            idx1 = i;
+            if (cards1[++i1] !== word) {
+                return "No";
+            }
         } else if (cards2.includes(word)) {
-            const i = cards2.indexOf(word);
-            if (idx2 !== i-1) { return "No"; }
-            idx2 = i;
+            if (cards2[++i2] !== word) {
+                return "No";
+            }
         } else {
             return "No";
         }
