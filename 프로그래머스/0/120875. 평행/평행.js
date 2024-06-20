f968f6935154a8a7762d8eb26e9ecd5f810fbbee
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-06-20 23:08:19 +0900

    [level 0] Title: 평행, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -5,21 +5,14 @@ function solution(dots) {
         - AC BD
         - AD BC
     */
-    
-    dots.sort((a, b) => {
-        if (a[0] !== b[0]) {
-            return a[0]-b[0];
-        } else {
-            return a[1]-b[1];
-        }
-    })
-    
+
     const slope = (i, j) => {
         const [xi, yi] = i;
         const [xj, yj] = j;
         
         return (yj-yi)/(xj-xi);        
     }
+    
     
     const [A, B, C, D] = dots;
     
