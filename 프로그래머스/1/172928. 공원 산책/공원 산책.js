98b8f1418c79563fa8267c5c64bbc94e926dfe0c
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-07-08 16:06:35 +0900

    [level 1] Title: 공원 산책, Time: 0.76 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -0,0 +1,52 @@
+function solution(park, routes) {
+    park = park.map(p => p.split(""));
+    
+    const [N, M] = [park.length, park[0].length];
+    let [x, y] = [0, 0];
+    
+    for (let i=0; i<N; i++) {
+        for (let j=0; j<M; j++) {
+            if (park[i][j] === "S") {
+                [x, y] = [i, j];
+            }
+        }
+    }
+    
+    const direction = {
+        "N": [-1, 0],
+        "S": [1, 0],
+        "W": [0, -1],
+        "E": [0, 1]
+    };
+    
+    const move = (x, y, mx, my, nx, ny) => {
+        while (true) {
+            if (x === nx && y === ny) break;
+            
+            [x, y] = [x+mx, y+my];
+            if (park[x][y] === "X") {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    for (route of routes) {
+        const [dir, dis] = route.split(" ");
+        const [mx, my] = direction[dir];
+        const [dx, dy] = [mx*Number(dis), my*Number(dis)];
+        const [nx, ny] = [x+dx, y+dy];
+        
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M || park[nx][ny] === "X") {
+            continue;
+        }
+        if (!move(x, y, mx, my, nx, ny)) {
+            continue;
+        }
+        
+        [x, y] = [nx, ny];
+    }
+    
+    return [x, y];
+}
