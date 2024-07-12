57b0c63b1e0df094c079417061bf64068f61ec1b
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-07-12 16:45:10 +0900

    [level 1] Title: 둘만의 암호, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,6 +1,27 @@
 function solution(s, skip, index) {
     let answer = "";
     
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
+    s.split("").forEach(a => {
+        let i = alpha.indexOf(a)+1;
+        let cnt = 0;
+        
+        while (cnt < index) {
+            if (i > 25) i -= 26;
+            
+            if (skip.indexOf(alpha[i]) !== -1) {
+                i++;
+                continue;
+            }
+            
+            i++;
+            cnt++;
+        }
+        
+        answer += alpha[--i];
+    })
+    
+    /*
     skip = skip.split("").map(sk => sk.charCodeAt(0)).sort((a, b) => a - b);
     
     s.split("").forEach(a => {
@@ -21,6 +42,7 @@ function solution(s, skip, index) {
         
         answer += String.fromCharCode(--cur);
     })
+    */
     
     return answer;
 }
