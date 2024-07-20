6fd66a1c6ca13894034fc7899d48db30fad5806e
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-07-21 03:48:19 +0900

    [level 1] Title: 옹알이 (2), Time: 0.66 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,23 @@
+function solution(babbling) {
+    const speaks = ["aya", "ye", "woo", "ma"];
+    
+    // 연속하는 문자열 찾기 ... ex: yeye
+    const arr = babbling.map(babble => {
+        for (speak of speaks) {
+            const i = babble.indexOf(speak.repeat(2));
+            if (i !== -1) {
+                return false;
+            }
+        }
+        
+        babble =  babble.split("aya").filter(b => b).join(" ")
+                        .split("ye").filter(b => b).join(" ")
+                        .split("woo").filter(b => b).join(" ")
+                        .split("ma").filter(b => b).join("")
+                        .split(" ").join("");
+        
+        return babble.length ? false : true;
+    })
+    
+    return arr.filter(v => v).length;
+}
