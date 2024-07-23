2386becb0ec13f60b92667d5af56c89f0d11edd8
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-07-23 14:27:23 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,36 @@
+function solution(dartResult) {
+    let score = [];
+    let N = 0;
+    let cur = -1;
+    
+    const bonus = {
+        "S": 1, "D": 2, "T":3
+    }
+    
+    dartResult.split("").forEach((v, i) => {
+        if (!isNaN(v)) {
+            if (v === "0" && cur !== -1) {
+                cur = 10;
+            } else {
+                cur = Number(v);
+            }
+        } else {
+            if (v === "S" || v === "D" || v === "T") {
+                score.push(cur ** bonus[v]);
+                N++;
+            } else {
+                if (v === "*") {
+                    if (N > 1) {
+                        score[N-2] *= 2;
+                    }
+                    score[N-1] *= 2;
+                } else {
+                    score[N-1] *= (-1);
+                }
+            }
+            cur = -1;
+        }
+    })
+    
+    return score.reduce((acc, cur) => acc+cur, 0);
+}
