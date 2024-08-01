4483b3925ac84d366e1a0e2ab666b83877e6d27d
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-08-01 13:32:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,21 @@
+function solution(n, words) {
+    let used = [];
+    let start = "";
+    
+    for (let i=0; i<words.length; i++) {
+        const word = words[i];
+        
+        if (
+            (i && start !== word[0]) || 
+            used.includes(word) ||
+            word.length === 1
+        ) {
+            return [i%n+1, parseInt(i/n)+1];
+        } else {
+            used.push(word);
+            start = word[word.length-1];
+        }   
+    }
+
+    return [0, 0];
+}
