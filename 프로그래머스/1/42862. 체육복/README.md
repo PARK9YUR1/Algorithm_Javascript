7c33264356834b7630c89dd167c7c81cb5c27e89
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-10-31 00:42:03 +0900

    [level 1] Title: 체육복, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.22 ms
+메모리: 33.4 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 28일 14:45:30
+2024년 10월 31일 00:42:01
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,19 +1,30 @@
-function solution(n, lost, reserve) {
-    const arr = lost.filter(l => reserve.includes(l));
-    lost = lost.filter(l => !arr.includes(l)).sort((a, b) => a - b);
-    reserve = Array(n+2).fill().map((_, i) => reserve.includes(i) && !arr.includes(i) ? 1 : 0);
+function solution(n, lost, reserve) {  // 학생수, 도난당한 학생번호, 여벌체육복 학생번호
+    let answer = n;
+    const lostAndReserve = reserve.filter(r => lost.includes(r));  // 도난도 당하고 여벌체육복도 있는 학생번호
+    lost = lost.filter(l => !lostAndReserve.includes(l)).sort((a, b) => a - b);
+    reserve = reserve.filter(r => !lostAndReserve.includes(r) && ((r > 1 && lost.includes(r-1)) || (r < n && lost.includes(r+1))))
+                     .sort((a, b) => a - b);
+    answer -= lost.length;
     
-    let answer = n - lost.length;
-    
-    for (i of lost) {
-        if (reserve[i-1]) {
-            reserve[i-1] = 0;
+    lost.forEach((num) => {
+        const idx1 = reserve.indexOf(num-1);
+        const idx2 = reserve.indexOf(num+1);
+        
+        if (num === 1 && idx2 > -1) {
             answer++;
-        } else if (reserve[i+1]) {
-            reserve[i+1] = 0;
+            reserve = reserve.slice(idx2+1);
+        } else if (num === n && idx1 > -1) {
             answer++;
+        } else {
+            if (idx1 > -1) {
+                answer++;
+                reserve = reserve.slice(idx1+1);
+            } else if (idx2 > -1) {
+                answer++;
+                reserve = reserve.slice(idx2+1);
+            }
         }
-    }
+    })
     
     return answer;
 }
