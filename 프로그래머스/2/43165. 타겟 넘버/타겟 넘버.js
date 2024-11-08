9dd85f3d2ddaabdadb04416097719d35e351d812
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-11-08 21:09:29 +0900

    [level 2] Title: 타겟 넘버, Time: 40.51 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,2 @@
 function solution(numbers, target) {
-    let answer = 0;
-    const length = numbers.length;
-
-    function dfs(i, sum) {
-      if (i === length) {
-        if (sum === target) {
-          answer++;
-        }
-        return;
-      }
-
-      dfs(i+1, sum-numbers[i]);
-      dfs(i+1, sum+numbers[i]);
-    }
-
-    dfs(0, 0);
-    
-    return answer;
 }
