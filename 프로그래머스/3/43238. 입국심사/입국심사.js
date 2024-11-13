bff4afbb84e3ee64dcfc1a69820b19bea348df0e
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2024-11-13 15:21:28 +0900

    [level 3] Title: 입국심사, Time: 923.39 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,21 @@
+function solution(n, times) {
+    times.sort((a, b) => a - b);
+    let [mn, mx] = [1, times[times.length - 1] * n];
+    
+    while (mn < mx) {
+        const mid = parseInt((mn + mx) / 2);
+        
+        // 심사 받을 수 있는 사람 수
+        const sum = times.reduce((acc, cur) => {
+            return acc + parseInt(mid / cur);
+        }, 0);
+        
+        if (sum >= n) {
+            mx = mid;
+        } else {
+            mn = mid + 1;
+        }
+    }
+    
+    return mx;
+}
