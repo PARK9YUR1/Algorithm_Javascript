27f0ad25c3f3ce8389cc0f55faf1724b2a91d60b
PARK9YUR1 <132658372+PARK9YUR1@users.noreply.github.com>
2025-01-06 22:58:30 +0900

    [level 2] Title: 피로도, Time: 8.66 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -3,20 +3,20 @@ function solution(k, dungeons) {
     let N = dungeons.length;
     let visited = Array(N).fill(false);
     
-    function dfs(curK, visited, count) {
+    function dfs(curK, count) {
         mx = Math.max(mx, count);
         
         for (let i=0; i<N; i++) {
             const [require, consume] = dungeons[i];
             if (!visited[i] && curK >= require) {  // 방문 x & 최소필요피로도 이상
                 visited[i] = true;
-                dfs(curK - consume, visited, count+1);
+                dfs(curK - consume, count+1);
                 visited[i] = false;
             }
         }
     }
     
-    dfs(k, visited, 0);
+    dfs(k, 0);
     
     return mx;
 }
